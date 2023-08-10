a5785a596e0c972ce62171c5f38ab3b310af4ab3
Abe <abe.m.mankavil@gmail.com>
2023-08-10 13:34:46 -0400

    Create Expression objects for each type of expr

---
+++ src/expr.py
@@ -0,0 +1,21 @@
+class Expr():
+    pass
+
+class Binary(Expr):
+    def __init__(self, expr_left, operator_token, expr_right):
+        self.expr_left = expr_left
+        self.operator_token = operator_token
+        self.expr_right = expr_right
+
+class Grouping(Expr):
+    def __init__(self, expression):
+        self.expression = expression
+
+class Literal(Expr):
+    def __init__(self, object_value):
+        self.object_value = object_value
+
+class Unary(Expr):
+    def __init__(self, operator_token, expr_right):
+        self.operator_token = operator_token
+        self.expr_right = expr_right
